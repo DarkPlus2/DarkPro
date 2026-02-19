d04869c6eea5beaa92d878bfbf72bbe350f44167
Dark <158550285+DarkPlus2@users.noreply.github.com>
2026-02-19 16:09:16 +0600

    Add Home component with FlexCard

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import FlexCard from './components/FlexCard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black p-4">
+      <FlexCard />
+    </main>
+  );
+}
